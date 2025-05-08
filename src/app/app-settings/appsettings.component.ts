c6c3027c5137ac1df2027853f756de240955513e
Maribel Pérez López <desarrolloti49_lem@castores.com.mx>
2025-05-08 09:40:06 -0600

    config: Se agregan archivos de configuración para transpilar proyecto.

--- src/app/app-settings/appsettings.component.ts
+++ src/app/app-settings/appsettings.component.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppsettingsComponent {
   API_ENDPOINT = 'https://back.castores.com.mx:443/castores/services/';
